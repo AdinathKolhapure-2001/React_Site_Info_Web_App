192fc3a77081d927f6aed135509bbaeb92a65072
AdinathKolhapure-2001 <76577037+AdinathKolhapure-2001@users.noreply.github.com>
2024-01-18 22:59:56 +0530

    Uploaded all the files

---
+++ Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img className = "nav--icon" src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"/>
+            <h3 className="nav--logo_text"> ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+    )
+}
